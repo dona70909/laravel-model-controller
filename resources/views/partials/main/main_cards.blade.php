ec28c9966d10ae721087090865ce0b55fe49c28d
Donatella7 <campaniellodonatella@gmail.com>
2022-05-12 15:43:24 +0200

    Movie properties

--- resources/views/partials/main/main_cards.blade.php
+++ resources/views/partials/main/main_cards.blade.php
@@ -2,10 +2,11 @@
     <div class="row p-4 movie-cards-wrap">
         @foreach ($movies as $movie)
             <div class="card col-2">
-                <img class="card-img-top" src="..." alt="Card image cap">
+                {{--  <img class="card-img-top" src="..." alt="Card image cap"> --}}
                 <div class="card-body">
-                    <h5 class="card-title">{{$movie->title}}</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    {{-- <h5 class="card-title">Title:</h5> --}}
+                    <p class="card-text">{{$movie->title}}</p>
+                    <p class="card-text">{{$movie->vote}}</p>
                 </div>
             </div>
         @endforeach
