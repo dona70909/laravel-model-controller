484016838415e1e2eb8a27d95f8e745e61a33656
Donatella7 <campaniellodonatella@gmail.com>
2022-05-12 15:11:48 +0200

    db link to controller

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Movie;
 
 class PageController extends Controller
 {
     public static function index() {
 
-        return view('guest.home.index');
+        $movies = Movie::all();
+
+        return view('guest.home.index',compact('movies'));
     }
 }
--- resources/views/partials/main/main_cards.blade.php
+++ resources/views/partials/main/main_cards.blade.php
@@ -1,11 +1,13 @@
 <section class="container-fluid">
     <div class="row p-4">
-        <div class="card col-2">
-            <img class="card-img-top" src="..." alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        @foreach ($movies as $movie)
+            <div class="card col-2">
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">{{$movie->title}}</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
             </div>
-        </div>
+        @endforeach
     </div>
 </section>
